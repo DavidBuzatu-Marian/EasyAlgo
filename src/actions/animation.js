c26c28931015771363802048d9b02f9010aeacb7
davidbuzatu-marian <davidm.buzatu@gmail.com>
2020-10-17 21:49:09 +0100

    Fixed bug on nav while animating

--- src/actions/animation.js
+++ src/actions/animation.js
@@ -1,9 +1,8 @@
-import {SET_ANIMATION_STATE} from './types'
+import { SET_ANIMATION_STATE } from './types';
 
-
-export const setAnimationState = (state) => async(dispatch) => {
-    dispatch({
-        type: SET_ANIMATION_STATE,
-        payload: state
-    })
-}
+export const setAnimationState = (state) => async (dispatch) => {
+  dispatch({
+    type: SET_ANIMATION_STATE,
+    payload: state,
+  });
+};
--- src/components/layout/DropdownBox.js
+++ src/components/layout/DropdownBox.js
@@ -5,6 +5,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { setBitsState } from '../../actions/bitsAnimation';
+import { setNavState } from '../../actions/navbar';
 import { BINARY_TYPE, NUMBER_TYPE } from '../utils/Constants';
 import IconNum from '../../images/NumbersMenu.svg';
 import IconBin from '../../images/BinaryMenu.svg';
@@ -16,17 +17,20 @@ const DropdownBox = ({
   animation: { isAnimated },
   setBitsState,
   bitsAnimation,
+  setNavState,
 }) => {
   const [dropDownState, setDropDownState] = useState({
     toggle: false,
   });
 
   const { toggle } = dropDownState;
+  const disabled = isAnimated || bitsAnimation.isAnimated;
   return (
     <div className='navbar-dropdown'>
       <div className='navbar-dropdown-header'>
         <Link to={link}>
           <button
+            disabled={disabled}
             className={toggle ? 'active' : ''}
             onClick={(e) => setDropDownState({ toggle: !toggle })}
           >
@@ -35,11 +39,12 @@ const DropdownBox = ({
               alt='Numbers Menu'
             />
             {sortingType}
-            {toggle ? (
-              <ChevronUpIcon size={16} verticalAlign='middle' />
-            ) : (
-              <ChevronDownIcon size={16} verticalAlign='middle' />
-            )}
+            {!disabled &&
+              (toggle ? (
+                <ChevronUpIcon size={16} verticalAlign='middle' />
+              ) : (
+                <ChevronDownIcon size={16} verticalAlign='middle' />
+              ))}
           </button>{' '}
         </Link>
       </div>
@@ -49,7 +54,7 @@ const DropdownBox = ({
             <button
               disabled={isAnimated || bitsAnimation.isAnimated}
               key={idx}
-              onClick={(e) =>
+              onClick={(e) => {
                 sortingType !== BINARY_TYPE
                   ? setAnimationState({
                       sortMethod: menu,
@@ -62,8 +67,12 @@ const DropdownBox = ({
                       elementsSize: 0,
                       animations: [],
                       start: true,
-                    })
-              }
+                    });
+                setNavState({
+                  activeCategory: sortingType,
+                  activeMethod: menu,
+                });
+              }}
             >
               {menu}
             </button>
@@ -81,6 +90,7 @@ DropdownBox.propTypes = {
   setBitsState: PropTypes.func.isRequired,
   animation: PropTypes.object.isRequired,
   bitsAnimation: PropTypes.object.isRequired,
+  setNavState: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => ({
@@ -88,6 +98,8 @@ const mapStateToProps = (state) => ({
   bitsAnimation: state.bitsAnimation,
 });
 
-export default connect(mapStateToProps, { setAnimationState, setBitsState })(
-  DropdownBox
-);
+export default connect(mapStateToProps, {
+  setAnimationState,
+  setBitsState,
+  setNavState,
+})(DropdownBox);
