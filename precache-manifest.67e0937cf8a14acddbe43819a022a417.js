257b35dd38d4d2676c6a8b8dfe4df035b5d8fdd8
davidbuzatu-marian <davidm.buzatu@gmail.com>
2020-10-18 22:38:06 +0100

    Updates

---
+++ precache-manifest.67e0937cf8a14acddbe43819a022a417.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b67b99fc9f8e6ec7a5e0c490dc9221d7",
+    "url": "/EasyAlgo/index.html"
+  },
+  {
+    "revision": "366eb5090d2373db9a49",
+    "url": "/EasyAlgo/static/css/main.1f78bd33.chunk.css"
+  },
+  {
+    "revision": "c7f073eef259f2e17892",
+    "url": "/EasyAlgo/static/js/2.b81a7be8.chunk.js"
+  },
+  {
+    "revision": "a3ca4fad6c20f8df27c31f653f9e89e9",
+    "url": "/EasyAlgo/static/js/2.b81a7be8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "366eb5090d2373db9a49",
+    "url": "/EasyAlgo/static/js/main.f9c2881b.chunk.js"
+  },
+  {
+    "revision": "d0551a71264c2ac1b3a0",
+    "url": "/EasyAlgo/static/js/runtime-main.96876d12.js"
+  },
+  {
+    "revision": "d2c2149b9a2168eae5ec6e7dee5ee313",
+    "url": "/EasyAlgo/static/media/BinaryMenu.d2c2149b.svg"
+  },
+  {
+    "revision": "20f2b23d41d3159deef8d3ee8aefd45c",
+    "url": "/EasyAlgo/static/media/NumbersMenu.20f2b23d.svg"
+  },
+  {
+    "revision": "50330caee5053fa3d4e4d6931423ac21",
+    "url": "/EasyAlgo/static/media/close.50330cae.svg"
+  },
+  {
+    "revision": "4235ed7881d945833628f1d10feba894",
+    "url": "/EasyAlgo/static/media/undraw_reading_time_gvg0.4235ed78.svg"
+  },
+  {
+    "revision": "34d6b0b45e7e07d0699b580f2664d7c3",
+    "url": "/EasyAlgo/static/media/undraw_sorting_thoughts_6d48.34d6b0b4.svg"
+  },
+  {
+    "revision": "0370826fa2f851ae116a4638c26ba7e1",
+    "url": "/EasyAlgo/static/media/undraw_visual_data_b1wx.0370826f.svg"
+  }
+]);
